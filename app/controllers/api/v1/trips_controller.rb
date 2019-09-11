2c555cc5f6044060868ac08cb57d9e3226820b3e
a.elanadoly <ah.elanadoly@gmail.com>
2019-09-11 03:16:31 +0200

    feat: Add privte mathod to generate time object

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -7,16 +7,30 @@ module Api
       end
 
       def show
-        @trip = Trip.find(params[:id])
-        @driver = @trip.driver
-        @truck = @trip.truck
-        @routes = @trip.routes.order("time")
-        start_time = @routes.first.time.to_i
-        end_time = @routes.last.time.to_i
+        @trip = Trip.find(params[:id]);
+        @driver = @trip.driver;
+        @truck = @trip.truck;
+        @routes = @trip.routes.order("time");
+        start_time = @routes.first.time.to_i;
+        end_time = @routes.last.time.to_i;
 
-        @trip_duration_by_minutes = (end_time - start_time) / 60;
+        trip_time = (end_time - start_time);
+        @trip_duration = seconds_to_units(trip_time);
 
-        render json: {status: 'SUCCESS', message:'Loaded truck', data:{trip: @trip, driver: @driver, truck: @truck, routes: @routes, trip_duration: @trip_duration_by_minutes}},status: :ok
+        render json: {status: 'SUCCESS', message:'Loaded truck', data:{trip: @trip, driver: @driver, truck: @truck, routes: @routes, trip_duration: @trip_duration}},status: :ok
+      end
+
+      private
+  
+      def seconds_to_units(seconds)
+        mm, ss = seconds.divmod(60);
+        hh, mm = mm.divmod(60);
+        return {
+          "hours": hh,
+          "minutes": mm,
+          "seconds": ss
+        }
+        
       end
       
     end
